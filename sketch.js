6d539b09bd348ad8ffb3ab472c1bdb37a370b754
Jayadityan1 <70211590+Jayadityan1@users.noreply.github.com>
2020-09-21 16:18:38 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -27,3 +27,11 @@ function draw() {
   }
   drawSprites();
 }
+function hasCollided(lbullet, lwall) {
+    bulletRightEdge = lbullet.x + lbullet.width;
+    wallLeftEdge = lwall.x
+    if (bulletRightEdge >= wallLeftEdge) {
+        return true;
+    }
+    return false;
+}
